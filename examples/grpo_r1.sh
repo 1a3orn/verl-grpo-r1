2fc7f49332370a730bcde5608e166c09bc9a6bc5
1a3orn <1a3orn@protonmail.com>
2025-02-08 16:44:31 -0500

    Changes.

--- examples/grpo_r1.sh
+++ examples/grpo_r1.sh
@@ -4,7 +4,7 @@ export VLLM_ATTENTION_BACKEND=XFORMERS
 
 python3 -m verl.trainer.main_grpo \
     algorithm.adv_estimator=grpo \
-    data.train_files=$DATA_DIR/all_geography_train.parquet \
+    data.train_files=$DATA_DIR/all_zebralogic_train.parquet \
     data.val_files=[$DATA_DIR/all_geography_test.parquet,$DATA_DIR/all_gsm8k_test.parquet,$DATA_DIR/all_trash_math_test.parquet,$DATA_DIR/all_zebralogic_test.parquet] \
     data.train_batch_size=100 \
     data.val_batch_size=20 \
@@ -14,7 +14,7 @@ python3 -m verl.trainer.main_grpo \
     actor_rollout_ref.actor.optim.lr=2e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=25 \
-    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=5 \
+    actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=10 \
     actor_rollout_ref.actor.use_kl_loss=True \
     actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
@@ -27,16 +27,16 @@ python3 -m verl.trainer.main_grpo \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.3 \
     actor_rollout_ref.rollout.n=8 \
-    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=4 \
+    actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=8 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
     critic.ppo_micro_batch_size=5 \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name="grpo_generalization" \
-    trainer.experiment_name="train_geography" \
+    trainer.experiment_name="train_zebralogic" \
     trainer.n_gpus_per_node=2 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
-    trainer.test_freq=5 \
+    trainer.test_freq=10 \
     trainer.total_epochs=15 2>&1 | tee verl_demo.log
