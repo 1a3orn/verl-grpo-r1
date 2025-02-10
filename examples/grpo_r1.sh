4aaea60ecf1676fe5f2ae155dc69ab097023b372
1a3orn <1a3orn@protonmail.com>
2025-02-10 15:24:01 -0500

    ASD

--- examples/grpo_r1.sh
+++ examples/grpo_r1.sh
@@ -11,7 +11,7 @@ python3 -m verl.trainer.main_grpo \
     data.max_prompt_length=512 \
     data.max_response_length=3400 \
     actor_rollout_ref.model.path="./models/Qwen2.5-3B-Instruct" \
-    actor_rollout_ref.actor.optim.lr=1e-6 \
+    actor_rollout_ref.actor.optim.lr=3e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=25 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=3 \
