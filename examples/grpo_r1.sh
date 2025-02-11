87bf01972dc6cfb84e08f36419c79b4748fdb41c
1a3orn <1a3orn@protonmail.com>
2025-02-11 10:58:07 -0500

    Doubling validation datasets.

--- examples/grpo_r1.sh
+++ examples/grpo_r1.sh
@@ -4,14 +4,14 @@ export VLLM_ATTENTION_BACKEND=XFORMERS
 
 python3 -m verl.trainer.main_grpo \
     algorithm.adv_estimator=grpo \
-    data.train_files=$DATA_DIR/all_zebralogic_train.parquet \
+    data.train_files=$DATA_DIR/all_gsmk8k_train.parquet \
     data.val_files=[$DATA_DIR/all_geography_test.parquet,$DATA_DIR/all_gsm8k_test.parquet,$DATA_DIR/all_trash_math_test.parquet,$DATA_DIR/all_zebralogic_test.parquet] \
-    data.train_batch_size=10 \
-    data.val_batch_size=20 \
+    data.train_batch_size=6 \
+    data.val_batch_size=6 \
     data.max_prompt_length=512 \
-    data.max_response_length=3400 \
+    data.max_response_length=3000 \
     actor_rollout_ref.model.path="./models/Qwen2.5-3B-Instruct" \
-    actor_rollout_ref.actor.optim.lr=3e-6 \
+    actor_rollout_ref.actor.optim.lr=4e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=25 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=3 \
@@ -26,11 +26,11 @@ python3 -m verl.trainer.main_grpo \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.3 \
-    actor_rollout_ref.rollout.n=8 \
+    actor_rollout_ref.rollout.n=16 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=3 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
-    critic.ppo_micro_batch_size=5 \
+    critic.ppo_micro_batch_size=3 \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
     trainer.project_name="grpo_generalization_2" \
@@ -38,5 +38,5 @@ python3 -m verl.trainer.main_grpo \
     trainer.n_gpus_per_node=2 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
-    trainer.test_freq=25 \
+    trainer.test_freq=50 \
     trainer.total_epochs=15 2>&1 | tee verl_demo.log
