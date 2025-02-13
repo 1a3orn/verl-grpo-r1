770f76f7b47d003c0b3863fbfac99d900dbf6c96
1a3orn <1a3orn@protonmail.com>
2025-02-13 15:47:36 -0500

    ASD

--- examples/grpo_r1.sh
+++ examples/grpo_r1.sh
@@ -4,19 +4,30 @@ export VLLM_ATTENTION_BACKEND=XFORMERS
 
 python3 -m verl.trainer.main_grpo \
     algorithm.adv_estimator=grpo \
-    data.train_files=$DATA_DIR/all_zebralogic_train.parquet \
-    data.val_files=[$DATA_DIR/all_geography_test.parquet,$DATA_DIR/all_gsm8k_test.parquet,$DATA_DIR/all_trash_math_test.parquet,$DATA_DIR/all_zebralogic_test.parquet] \
+    data.train_files=$DATA_DIR/all_zebralogic_train_True.parquet \
+    data.val_files=[ \
+        $DATA_DIR/all_geography_test_True.parquet, \
+        $DATA_DIR/all_gsm8k_test_True.parquet, \
+        $DATA_DIR/all_trash_math_test_True.parquet, \
+        $DATA_DIR/all_zebralogic_test_True.parquet, \
+        $DATA_DIR/all_var_math_test_True.parquet \
+        $DATA_DIR/all_geography_test_False.parquet, \
+        $DATA_DIR/all_gsm8k_test_False.parquet, \
+        $DATA_DIR/all_trash_math_test_False.parquet, \
+        $DATA_DIR/all_zebralogic_test_False.parquet, \
+        $DATA_DIR/all_var_math_test_False.parquet \
+    ] \
     data.train_batch_size=6 \
     data.val_batch_size=6 \
     data.max_prompt_length=512 \
     data.max_response_length=4000 \
     actor_rollout_ref.model.path="./models/Qwen2.5-3B-Instruct" \
-    actor_rollout_ref.actor.optim.lr=4e-6 \
+    actor_rollout_ref.actor.optim.lr=2e-6 \
     actor_rollout_ref.model.use_remove_padding=True \
     actor_rollout_ref.actor.ppo_mini_batch_size=25 \
     actor_rollout_ref.actor.ppo_micro_batch_size_per_gpu=3 \
     actor_rollout_ref.actor.use_kl_loss=True \
-    actor_rollout_ref.actor.kl_loss_coef=0.0002 \
+    actor_rollout_ref.actor.kl_loss_coef=0.001 \
     actor_rollout_ref.actor.kl_loss_type=low_var_kl \
     actor_rollout_ref.model.enable_gradient_checkpointing=True \
     actor_rollout_ref.actor.fsdp_config.param_offload=False \
@@ -26,7 +37,7 @@ python3 -m verl.trainer.main_grpo \
     actor_rollout_ref.rollout.tensor_model_parallel_size=2 \
     actor_rollout_ref.rollout.name=vllm \
     actor_rollout_ref.rollout.gpu_memory_utilization=0.3 \
-    actor_rollout_ref.rollout.n=16 \
+    actor_rollout_ref.rollout.n=12 \
     actor_rollout_ref.ref.log_prob_micro_batch_size_per_gpu=3 \
     actor_rollout_ref.ref.fsdp_config.param_offload=True \
     algorithm.kl_ctrl.kl_coef=0.001 \
@@ -34,10 +45,10 @@ python3 -m verl.trainer.main_grpo \
     critic.ppo_mini_batch_size=3 \
     trainer.critic_warmup=0 \
     trainer.logger=['console','wandb'] \
-    trainer.project_name="grpo_generalization_2" \
-    trainer.experiment_name="train_zebralogic_04" \
+    trainer.project_name="grpo_generalization_3" \
+    trainer.experiment_name="train_zebralogic_01" \
     trainer.n_gpus_per_node=2 \
     trainer.nnodes=1 \
     trainer.save_freq=-1 \
-    trainer.test_freq=25 \
+    trainer.test_freq=30 \
     trainer.total_epochs=15 2>&1 | tee verl_demo.log
